4f10068ae35075e16b80169b58acd1357b282731
Stanislav Rogov <stanislavrogov68@gmail.com>
2026-04-14 22:04:53 +0300

    Add embedding aggregation method

--- src/data_pipline/processors/embedding_engine.py
+++ src/data_pipline/processors/embedding_engine.py
@@ -1,4 +1,5 @@
-from typing import List, Tuple
+from typing import List, Tuple, Optional
+import numpy as np
 from pgvector.sqlalchemy import Vector
 
 from src.core.logger import setup_logger
@@ -24,14 +25,19 @@ class EmbedderProcessor:
         logger.info(f"Indexed {len(ids)} chunks")
         return upload_data
 
-    async def run_company_embedding(self, records: List[Tuple[str, str]], batch_size=5):
-        logger.info("Starting form10 embedding process")
-
-        ids = [r['cik'] for r in records]
-        texts = [r['summary'] for r in records]
+    async def run_form10_embedding(self, records: List[Tuple[str, str]]) -> List[Tuple[str, Vector]]:
+        ids = [r[0] for r in records]
+        texts = [r[1] for r in records]
 
         vectors = await self.embedder.get_embeddings(texts)
         upload_data = list(zip(ids, vectors))
-        self.repo.save_form10_embeddings(upload_data)
 
-        logger.info(f"Indexed {len(ids)} forms")
+        logger.info(f"Indexed {len(ids)} form10")
+        return upload_data
+
+    def aggregate_embeddings(self, embeddings_list: List[List[float]]) -> Optional[List[float]]:
+        if not embeddings_list:
+            return None
+        embeddings_array = np.array(embeddings_list)
+        mean_embedding = np.mean(embeddings_array, axis=0)
+        return mean_embedding.tolist()
